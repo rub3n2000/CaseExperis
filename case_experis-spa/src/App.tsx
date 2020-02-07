8b84d274f75ff0fc28f70eadfd0d84f737ff870d
rub3n2000 <ruben@tresam.no>
2020-02-07 10:59:11 +0100

    Initialized Spa

---
+++ case_experis-spa/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      
+    </div>
+  );
+}
+
+export default App;
