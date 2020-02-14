8919990c4193fc1fc6a67db32d27cf1ea161f874
rub3n2000 <ruben@tresam.no>
2020-02-14 09:35:23 +0100

    Added logo and favicon.

--- case_experis-spa/src/Containers/Nav/Logo/Logo.tsx
+++ case_experis-spa/src/Containers/Nav/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import styles from './Logo.module.scss';
 const logo = ( props: any ) => {
     return (
         <div className={styles.Logo}>
-           <a href="/"><img src={window.location.origin + '/logo192.png'}/></a>
+           <a href="/"><img src={window.location.origin + '/logo128.png'}/></a>
         </div>
     )
 }
